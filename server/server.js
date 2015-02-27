6169e332103725f32c973b72752cfba2d7fd6442
Peter Sutton <foxxy@foxdogstudios.com>
2015-02-27 20:23:01 +0000

    Formatting

--- server/server.js
+++ server/server.js
@@ -123,12 +123,12 @@ function updateAnimatable(delta, newEntity, oldEntity) {
 
 function detectAndHandleCollisions(entities) {
 
-    for (var i = 1; i < entities.length; i++) {
-
+    for (var i = 1; i < entities.length; i++)
+    {
         var e1 = entities[i];
 
-        for (var j = 0; j < i; j++) {
-
+        for (var j = 0; j < i; j++)
+        {
             var e2 = entities[j];
 
             if (e1.newVersion._id === e2.newVersion._id)
